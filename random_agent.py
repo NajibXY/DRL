3731d883c5357998bd1bde03b3109687bbd50e51
najib elkhadir <naj.elkhadir@gmail.com>
2021-01-05 20:34:02 +0100

    random agent with accumulated reward display

--- random_agent.py
+++ random_agent.py
@@ -1,8 +1,8 @@
 import argparse
-import sys
 import torch
 import gym
 from gym import wrappers, logger
+import matplotlib.pyplot as plt
 
 class RandomAgent(object):
     """The world's simplest agent!"""
@@ -13,8 +13,6 @@ class RandomAgent(object):
         return self.action_space.sample()
 
 if __name__ == '__main__':
-    print("TORCH = ", torch.__version__)
-    print("GYM = ", gym.__version__)
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('env_id', nargs='?', default='CartPole-v1', help='Select the environment to run')
     args = parser.parse_args()
@@ -38,16 +36,30 @@ if __name__ == '__main__':
     reward = 0
     done = False
 
+    rewards = []
+
     for i in range(episode_count):
         ob = env.reset()
+        step = 0
+        reward_tot = 0
         while True:
             action = agent.act(ob, reward, done)
             ob, reward, done, _ = env.step(action)
+            reward_tot = reward_tot + reward
             if done:
                 break
             # Note there's no env.render() here. But the environment still can open window and
             # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
             # Video is not recorded every episode, see capped_cubic_video_schedule for details.
+            rewards.append(reward_tot)
+
+    # Affichage de l'évolution de la récompense
+    fig = plt.figure()
+    fig.suptitle('Récompense cumumée par épisode', fontsize=12)
+    plt.xlabel('Episode', fontsize=10)
+    plt.ylabel('Total de la récompense cumulée', fontsize=10)
+    plt.plot(rewards)
+    plt.show()
 
     # Close the env and write monitor result info to disk
     env.close()
